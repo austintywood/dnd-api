e0fee8f7f3cbf9c855a545dd4ae66b1c72089533
Austin Wood <austin.ty.wood@gmail.com>
2023-11-20 15:58:25 -0800

    Added sqlalchemy engine

--- src/database.py
+++ src/database.py
@@ -1,4 +1,16 @@
 import os
 import dotenv
+from sqlalchemy import create_engine
+from sqlalchemy import URL
 
 dotenv.load_dotenv()
+
+db_url = URL.create(
+    drivername='postgresql',
+    username=os.getenv('DATABASE_USERNAME'),
+    password=os.getenv('DATABASE_PASSWORD'),
+    host=os.getenv('DATABASE_HOST'),
+    port=os.getenv('DATABASE_PORT'),
+    database=os.getenv('DATABASE_NAME'),
+)
+engine = create_engine(db_url)
