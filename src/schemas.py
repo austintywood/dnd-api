ae0b61c7c166730fb74198d29c09122ea64987a5
Austin Wood <austin.ty.wood@gmail.com>
2023-11-20 16:17:33 -0800

    Added user schemas

--- src/schemas.py
+++ src/schemas.py
@@ -1,6 +1,23 @@
 from pydantic import BaseModel
 
 
+
 class CharacterBase(BaseModel):
     name: str
     age: int | None = None
+
+class UserBase(BaseModel):
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    is_active: bool
+
+    class Config:
+        from_attributes = True
+
+class UserDelete(User):
+    pass
